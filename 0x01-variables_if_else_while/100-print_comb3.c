98c09d8d022133faff22349c03448a7826d73989
amedeecedrick <iradukundacedrick4@gmail.com>
2023-02-17 09:36:06 -0800

    magic

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,21 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			putchar(',');
+			if (i != 9 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');

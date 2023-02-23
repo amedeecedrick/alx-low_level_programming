bd891555591d5ddb2dffa71fb8003f3471048052
amedeecedrick <iradukundacedrick4@gmail.com>
2023-02-23 05:11:22 -0800

    test

---
+++ 0x03-debugging/test/work.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main ()
+{
+	int i;
+
+i = 9;
+while (i--)
+{
+    printf("%d", i);
+}
+        return (0);
+}
